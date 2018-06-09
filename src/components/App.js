0d820e069caad81c07e7419452755d58f6eea86e
mercedesb <mercedesbernard@live.com>
2018-06-09 18:46:30 -0500

    file/folder restructuring

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import '../styles/App.css';
 import { WordBank } from './WordBank'
 import { PuzzleGenerator } from './PuzzleGenerator'
 
